5936e6eb9cad0cb2306497e8ab40d319cc4e3b8d
BrandonConte <bconte950@gmail.com>
2022-01-05 20:54:18 -0500

    middleware added to check user's cookie data

--- server.js
+++ server.js
@@ -33,4 +33,15 @@ app.use(session({
 }));
 
 // handlebars config.
+// Using the handlebars app (hbs) we want a default layout - located in view/layouts/
+app.engine('hbs', hbs({extname: 'hbs',defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'})); 
+app.set('view engine', 'hbs');
 
+// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
+// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');
+    }
+    next();
+});
