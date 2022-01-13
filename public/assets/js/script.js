556171f88de7a9a13b3f8ddbd8e659ea73adb9f1
BrandonConte <bconte950@gmail.com>
2022-01-12 19:06:25 -0500

    attempting to fix issues with js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,13 +1,24 @@
 let catGifApiKey = "a41b1fc9-3c83-4302-a5d5-4f2c67c5c244";
-let catGifUrl = "https://api.thecatapi.com/v1/images/search?mime_types=gif";
 let catBreedApi = "a41b1fc9-3c83-4302-a5d5-4f2c67c5c244";
 let catBreedUrl = "https://api.thecatapi.com/v1/breeds?";
+let catFactUrl = "https://catfact.ninja/fact?limit=1&max_length=140";
+let catGifUrl = "https://api.thecatapi.com/v1/images/search?mime_types=gif";
+let factContainer = document.getElementById("fact-container")
+let factBtn = document.getElementById("factBtn");
+let gifContainer = document.getElementById("catGif")
+let gifBtn = document.getElementById("gifBtn");
 let breedList = document.getElementById("breed-list");
 let breedImgContainer = document.getElementById("breedImg");
 let breedInfoContainer = document.getElementById("breedInfo");
 
 gifBtn.addEventListener('click', returnCatGif);
 factBtn.addEventListener('click', returnCatFact);
+<<<<<<< HEAD
+=======
+// breedNameEl.addEventListener("click", function (event) {
+//     returnCatGif(event.target.id);
+// })
+>>>>>>> parent of 084ba49 (Revert "code refactor, random gif/fact html/js")
 
 // function to display a random cat fact 
 function returnCatFact() {
@@ -27,81 +38,82 @@ function returnCatFact() {
 };
 
 // return cat pictures from cat gif api
-
 function returnCatGif() {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "image/");
     myHeaders.append("Authorization", "a41b1fc9-3c83-4302-a5d5-4f2c67c5c244");
-}
-fetch(catGifUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (response) {
-        console.log(response);
 
-        for (var i = 0; i < response.length; i++) {
-            {
-
-                var responseContainerEl = document.querySelector('#catGif');
-                responseContainerEl.innerHTML = '';
-
-                var catImg = document.createElement('img');
-                catImg.setAttribute("style", "width:450px; hight:450px; ");
-                catImg.setAttribute('src', response[i].url);
-
-                //   Append 'catImg' to the <div>
-                responseContainerEl.appendChild(catImg);
+    gifContainer.textContent = ''
+    fetch(catGifUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response)
+            for (var i = 0; i < response.length; i++) {
+                {
+                    var catGif = document.createElement('img')
+                    catGif.setAttribute("style", "width:450px; height:450px; ");
+                    catGif.setAttribute('src', response[i].url);
+                    gifContainer.appendChild(catGif);
+                }
             }
-        }
-    })
-
-
-// function returnBreedGif() {
-
-//   var gifByBreed = data[i].id
-//   var breedUrl = "https://api.thecatapi.com/v1/images/search?breed_id=" + gifByBreed;
-
-//   fetch(breedUrl)
-//   .then(function(response){
-// if (response.ok){
-//   return response.json()
-
-//   .then(function(data) {
-//     console.log(data);
-//   })
-// }
-// else {
-//   alert("fetch error");
-// }
-//   }
-
-//   )}
+        });
+};
 
 function returnCatBreed() {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     myHeaders.append("x-api-key", "a41b1fc9-3c83-4302-a5d5-4f2c67c5c244");
+<<<<<<< HEAD
 }
 
 // calls the catAPI to display breed names in the drop down
+=======
+};
+
+// return breed names from catAPI
+>>>>>>> parent of 084ba49 (Revert "code refactor, random gif/fact html/js")
 fetch(catBreedUrl + catBreedApi)
     .then(function (response) {
         return response.json();
     })
-
     .then(function (data) {
         console.log(data);
 
-
-        // get list of names from api and append it to the search bar
+        // for loop that creates each breed option
         for (var i = 0; i < data.length; i++) {
             let breedNameEl = document.createElement("option");
             breedNameEl.setAttribute("id", data[i].name);
             breedNameEl.textContent = data[i].name;
             breedList.appendChild(breedNameEl);
+<<<<<<< HEAD
+=======
+            console.log(breedNameEl);
         }
     });
+
+// function to display cat breed information by click
+function displayCatBreed() {
+    breedImgContainer.textContent = ''
+    breedInfoContainer.textContent = ''
+    fetch(catBreedUrl + catBreedApi)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+        console.log(data);
+        
+        
+        for (var i = 0; i < data.length; i++) {
+            var breedImg = document.createElement('img')
+                breedImg.setAttribute("style", "width:450px; height:450px; ");
+                breedImg.setAttribute('src', data.image);
+                breedImgContainer.appendChild(breedImg);
+>>>>>>> parent of 084ba49 (Revert "code refactor, random gif/fact html/js")
+        }
+    });
+};
 
 // function that gets clicked cat breed from the drop down
 // function getCatBreed() {
@@ -159,7 +171,7 @@ fetch(catBreedUrl + catBreedApi)
 
 
 
-//results based on breed
+// results based on breed
 
 function displayCatBreed() {
     var selectedValue = document.getElementById("breed-list").value
@@ -176,10 +188,19 @@ function displayCatBreed() {
             console.log(data);
 
             for (var i = 0; i < data.length; i++) {
+<<<<<<< HEAD
             var breedImg = document.createElement('img')
             breedImg.setAttribute("style", "width:450px; height:450px; ");
             breedImg.setAttribute('src', 'https://cdn2.thecatapi.com/images/' + data[i].reference_image_id + '.jpg');
             breedImgContainer.appendChild(breedImg);
+=======
+                let catNameEl = data[i].name;
+
+                console.log(catNameEl);
+                // if (selectedValue = catNameEl) {
+                //     console.log("match");
+                // }
+>>>>>>> parent of 084ba49 (Revert "code refactor, random gif/fact html/js")
             }
         })
     
@@ -242,21 +263,3 @@ var requestOptions = {
     headers: myHeaders,
     redirect: 'follow'
 };
-
-fetch("https://catfact.ninja/fact?limit=1&max_length=140", requestOptions)
-    .then(response => response.text())
-    .then(result => {
-        showResult = result => {
-            var catFactSplit = result.split(/[""""""]/);
-            let catFact = document.createElement("p");
-            catFact.innerHTML = catFactSplit[3];
-            catFact.className = "cat-fact-text";
-            console.log(catFact);
-            let catFactContainer = document.querySelector('#cat-fact');
-            catFactContainer.appendChild(catFact);
-        }
-        showResult(result);
-    })
-
-
-
