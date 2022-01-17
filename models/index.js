d101529af8546b2155190c8ff479d2cfc02a1320
Chice <cko517@hotmail.com>
2022-01-16 20:50:35 -0500

    file delete

--- models/index.js
+++ models/index.js
@@ -1,13 +1,3 @@
 const User = require('./User');
-const Search = require('./Search');
-const CatPics = require('./CatPics');
 
-Search.hasMany(CatPics, {
-    foreignKey: '',
-});
-
-CatPics.belongsTo(Search, {
-    foreignKey: '',
-});
-
-module.exports = { User, Search, CatPics };
+module.exports = { User };
